be681d390c9b91aecb9c253a6cd6e57aa1c34d00
Sami-Ullah-Tufail <sami.u.tufail@gmail.com>
2024-05-01 00:44:08 +0500

    deployment fix

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,58 +1,42 @@
 import { PutObjectCommandOutput, S3 } from "@aws-sdk/client-s3";
-import { uploadToS3 } from "../../lib/s3"; // Assuming the path to your s3 file
 
-jest.mock("@aws-sdk/client-s3", () => {
-  // Mock implementations for S3 and its methods
-  const mockPutObject = jest.fn();
-  const mockS3 = jest.fn(() => ({
-    putObject: mockPutObject,
-  }));
-  return { S3: mockS3 };
-});
-describe("uploadToS3", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
+export async function uploadToS3(
+  file: File
+): Promise<{ file_key: string; file_name: string }> {
+  return new Promise((resolve, reject) => {
+    try {
+      const s3 = new S3({
+        region: "eu-north-1",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
+        },
+      });
+
+      const file_key =
+        "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+        Body: file,
+      };
+      s3.putObject(
+        params,
+        (err: any, data: PutObjectCommandOutput | undefined) => {
+          return resolve({
+            file_key,
+            file_name: file.name,
+          });
+        }
+      );
+    } catch (error) {
+      reject(error);
+    }
   });
+}
 
-  it("should upload a file to S3 and return the file key and name", async () => {
-    // Prepare fake file data
-    const mockFile = new File(["test content"], "test.txt", {
-      type: "text/plain",
-    });
-
-    // Mock the response from S3.putObject
-    const mockPutObjectResponse = { Key: "uploads/..." }; // Or another suitable response
-    mockPutObject.mockResolvedValue(mockPutObjectResponse);
-
-    // Call your function
-    const result = await uploadToS3(mockFile);
-
-    // Assertions
-    expect(result).toEqual({
-      file_key: expect.stringMatching(/^uploads\/\d+/), // Check for the pattern
-      file_name: "test.txt",
-    });
-    expect(mockS3).toBeCalledWith({
-      region: "eu-north-1",
-      credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
-      },
-    });
-    expect(mockPutObject).toHaveBeenCalledWith({
-      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-      Key: expect.stringMatching(/^uploads\/\d+/),
-      Body: mockFile,
-    });
-  });
-
-  it("should reject the promise if an error occurs during upload", async () => {
-    const mockFile = new File(["test content"], "test.txt", {
-      type: "text/plain",
-    });
-    const mockError = new Error("Upload failed");
-    mockPutObject.mockRejectedValue(mockError);
-
-    await expect(uploadToS3(mockFile)).rejects.toThrow(mockError);
-  });
-});
+export function getS3Url(file_key: string) {
+  const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.eu-north-1.amazonaws.com/${file_key}`;
+  return url;
+}
